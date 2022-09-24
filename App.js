3815311c13d8e46b388fd81ab57b490b8f335f06
Eugenio Rosario Mbi <eugenio.dorosario94@gmail.com>
2022-09-24 18:35:21 -0300

    feat: handling splashcreen and custom font before the is ready

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { StyleSheet, SafeAreaView, StatusBar, View } from 'react-native';
+import * as SplashScreen from "expo-splash-screen";
+
 import Routes from '@routes';
 import { isAndroid } from '@utils/constants';
+import fontsSetup from 'setup/fonts';
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await fontsSetup();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) await SplashScreen.hideAsync();
+  }, [isReady]);
+
+  if (!isReady) return null;
+
   return (
     <SafeAreaView style={styles.safeArea}>
-      <StatusBar style="auto" />
-      <Routes />
+      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+        <StatusBar style="auto" />
+        <Routes />
+      </View>
     </SafeAreaView>
   );
 }
