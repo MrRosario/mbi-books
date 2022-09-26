f0013c100edcd7dfed5f7e6f9a8f8bb6e61a0974
Eugenio Rosario Mbi <eugenio.dorosario94@gmail.com>
2022-09-26 15:02:23 -0300

    refactor: borderRadius contanstant added

--- src/styles/spacing.ts
+++ src/styles/spacing.ts
@@ -1,6 +1,12 @@
 import { scaleSize } from "./mixins";
 
-export const SCALE_18 = scaleSize(18);
-export const SCALE_16 = scaleSize(16);
-export const SCALE_12 = scaleSize(12);
-export const SCALE_8 = scaleSize(8);
+const Spacing = {
+    SCALE_8: scaleSize(8),
+    SCALE_12: scaleSize(12),
+    SCALE_16: scaleSize(16),
+    SCALE_18: scaleSize(18),
+    SCALE_24: scaleSize(24),
+    BORDER_RADIUS: 4
+}
+
+export default Spacing;
