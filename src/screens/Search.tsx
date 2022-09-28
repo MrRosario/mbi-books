b753b21f833737bccc5c819b095b5a113e35ed03
Eugenio Rosario Mbi <eugenio.dorosario94@gmail.com>
2022-09-28 00:02:57 -0300

    feat: performing book searching

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -1,10 +1,104 @@
-import React from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, TextInput, FlatList } from 'react-native';
+import { Colors, Font, Spacing } from "styles";
 
-const Search = () => (
-    <View>
-        <Text>SearchScreen</Text>
-    </View>
-);
+import { useQuery } from 'react-query';
+import { searchBook } from '@services/api'
+import Layout from "components/Layout";
+import Label from "components/Label";
+import NotFound from "components/NotFound";
+import Spinner from "components/Spinner";
+import Card from "components/Card";
+import useDebounce from "hooks/useDebounce";
 
+const Search = ({ navigation }: any) => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+    // const [searchResults, setSearchResults] = useState<any>([]);
+    const [page, setPage] = useState<number>(0);
+
+    const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+    const { isLoading, data, refetch } = useQuery(
+        ["book", debouncedSearchTerm], async () => 
+        await searchBook(debouncedSearchTerm, page)
+    );
+
+    const searchResults = data?.items;
+    const hasData = searchResults?.length > 0;
+
+    console.log('searchResults: ', searchResults)
+
+    const renderItem = ({ item }: any) => {
+        const { authors, publisher, title, imageLinks } = item?.volumeInfo;
+
+        return (
+            <Card 
+                navigation={navigation}
+                title={title}
+                author={authors} 
+                publisher={publisher} 
+                id={item.id}
+                imgUrl={imageLinks?.thumbnail}
+            />
+        );
+    }
+
+    return (
+        <Layout>
+            <Label black bigTitle label='Buscar' />
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.textInputStyle}
+                    placeholderTextColor={Colors.TRANSPARENT}
+                    onChangeText={(text) => setSearchTerm(text)}
+                    value={searchTerm}
+                    underlineColorAndroid="transparent"
+                    placeholder="Busque livros"
+                />
+
+                <View style={ styles.contentContainer}>
+                    { !hasData && isLoading && <Spinner /> }
+
+                    { !hasData && !isLoading && (
+                        <NotFound>
+                            <Label style={styles.text} 
+                                label='Por favor, use a barra de busca para encontrar o livro em que procuras.' 
+                            />
+                        </NotFound>
+                    )}
+                    { hasData && (
+                        <FlatList
+                            data={searchResults}
+                            renderItem={renderItem}
+                            keyExtractor={(item, index) => item.id}
+                        />
+                    )}
+                </View>
+            </View>
+        </Layout>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 30,
+    },
+    contentContainer: {
+        paddingTop: 40,
+    },
+    text: {
+        textAlign: 'center',
+        fontFamily: Font.FAMILY.THIN,
+        fontSize: 18
+    },
+    textInputStyle: {
+        width: '100%',
+        height: 50,
+        borderWidth: 1,
+        borderColor: Colors.TRANSPARENT,
+        borderRadius: Spacing.BORDER_RADIUS,
+        paddingHorizontal: 10,
+    }
+})
 export default Search;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,27 +1,7 @@
 import { API_KEY, BASE_URL } from "./apiConfig";
 
 const getBookDetails = (id:string) => 
-    fetch(`${BASE_URL}/${id}?key=${API_KEY}`)
-        .then((response) => response.json())
-        .then((json) => {
-        return json;
-        })
-        .catch((error) => {
-        console.error(error);
-        });
-
-const searchBook = (searchTerm:string) => 
-    fetch(`${BASE_URL}?q=${searchTerm}&key=${API_KEY}`)
-        .then((response) => response.json())
-        .then((json) => {
-        return json;
-        })
-        .catch((error) => {
-        console.error(error);
-        });
-
-const featuredBooks = () => 
-    fetch(`${BASE_URL}?q=emprendedorismo&orderBy=newest&maxResults=10&key=${API_KEY}`)
+  fetch(`${BASE_URL}/${id}?key=${API_KEY}`)
     .then((response) => response.json())
     .then((json) => {
       return json;
@@ -29,6 +9,26 @@ const featuredBooks = () =>
     .catch((error) => {
       console.error(error);
     });
+
+const searchBook = (searchTerm:String, page: Number) => 
+  fetch(`${BASE_URL}?q=${searchTerm}&startIndex=${page}&maxResults=10&key=${API_KEY}`)
+    .then((response) => response.json())
+    .then((json) => {
+      return json;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+const featuredBooks = () => 
+  fetch(`${BASE_URL}?q=emprendedorismo&orderBy=newest&maxResults=10&key=${API_KEY}`)
+  .then((response) => response.json())
+  .then((json) => {
+    return json;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 export {
     getBookDetails,
